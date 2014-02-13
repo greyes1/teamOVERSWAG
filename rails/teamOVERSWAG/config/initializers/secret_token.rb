fc4b7f1a0a62c558ba032c5e45fdeab122da320f
c <c>
2014-02-13 13:09:20 -0600

    added landing page

---
+++ rails/teamOVERSWAG/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamOVERSWAG::Application.config.secret_token = 'dc977f158b2dc9102782f6a7a59c7cfe1f6845ff7032fdc814dfe65e2f59fc1ebca0030fb049f3c0cd2646f152ca2f67d68aa44a01806acc5aaa723dd6c95b67'
